3f354cf667198d74b847dc7dc79ee0b8ffca0d72
mnezmah <mario.nezmah@gmail.com>
2019-06-26 21:02:10 +0200

    comented post request

--- team/router.js
+++ team/router.js
@@ -4,8 +4,6 @@ const Team = require('./model')
 const router = new Router()
 
 
-
-
 router.get(
   '/team',
   (req, res, next) =>
@@ -15,19 +13,19 @@ router.get(
       .catch(err => next(err))
 )
 
-router.post('/team', (req, res) => {
-  Team.create(req.body)
-    .then(newTeam => {
-      if (newTeam.name) {
-        return res.status(200)
-          // .json({ team })
-          .send(newTeam)
-      } next()
-    })
-    .catch(err =>
-      res.status(500).send(next(err))
-    )
-})
+// router.post('/team', (req, res) => {
+//   Team.create(req.body)
+//     .then(newTeam => {
+//       if (newTeam.name) {
+//         return res.status(200)
+//           // .json({ team })
+//           .send(newTeam)
+//       } next()
+//     })
+//     .catch(err =>
+//       res.status(500).send(next(err))
+//     )
+// })
 
 // router.post('/team', (req, res, next) => {
 //   Team.create(req.body)
