6754aa84064a8da30765c07fa16b9cbeff87e393
mnezmah <mario.nezmah@gmail.com>
2019-06-26 20:32:43 +0200

    added router.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const db = require ('./db.js')
 const Team = require ('./team/model.js')
+const teamRouter = require ('./team/router') 
+const jsonParser = require ('body-parser')
 const app = express()
 
 
+app.use(teamRouter)
+app.use(jsonParser)
 
-const port = process.env.PORT || 4001
-app.listen(port, () => console.log('Listening on port ${port}'))
+const port = process.env.PORT || 4000
+app.listen(port, () => `Listening on port ${port}`)
--- team/router.js
+++ team/router.js
@@ -1,10 +1,46 @@
-const Router = require('Router')
+const { Router } = require('express')
 const Team = require('./model')
 
-Router.get('/team', (req, res) => {
+const router = new Router()
+
+// router.get('/team', (request, response) => response.send(Team.findAll()))
+// router.post('/team', (request, response) => response.send(Team.create()))
+
+router.get('/team', (req, res, next) => {
   Team.findAll()
-  .then( teams => {
-    res.json({teams})
-  } )
-  .catch()
+    .then(teams => {
+      res.status(200)
+        .json({ teams })
+        .send(teams)
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
+
+router.post('/team', (req, res) => {
+  Team.create(req.body)
+    .then(newTeam => { if(newTeam.name)
+      res.status(200)
+        .json({ team })
+        .send(newTeam)
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
+})
+
+// router.post('/team', (req, res, next) => {
+//   Team.create(req.body)
+//       .then(newTeam => {
+//         if(newTeam.name) { return res.status(200).send(newTeam) }
+//         next()
+//       })
+//       .catch(err => res.status(500).send(next(err)))
+// })
+
+module.exports = router
