35b9576f4ced10033c3132d888bdbe61cfd23f7b
mnezmah <mario.nezmah@gmail.com>
2019-06-26 15:29:44 +0200

    added db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+// const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
+
+const sequelize = new Sequelize(databaseUrl)
+
+sequelize
+  .sync()
+  .then(() => conslole.log('db connected'))
+  .catch(() => console.error )
+
+  module.exports = sequelize
