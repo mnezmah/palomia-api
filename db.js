6f37f82a1704c329656da45beb3bdaeaf22999b7
mnezmah <mario.nezmah@gmail.com>
2019-06-26 15:35:37 +0200

    start script modified

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-// const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
-const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+// const databaseUrl = 'postgres://postgres:secret@localhost:5432/postgres'
 
 const sequelize = new Sequelize(databaseUrl)
 
