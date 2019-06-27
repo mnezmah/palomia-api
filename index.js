9e88102e6957a7e8d571d55c42330d670134413f
mnezmah <mario.nezmah@gmail.com>
2019-06-27 11:11:54 +0200

    declared jsonParser

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const jsonParser = require('body-parser')
+const bodyParser = require('body-parser')
 const loginRouter = require('./')
 
 const db = require ('./db.js')
@@ -8,13 +8,14 @@ const teamRouter = require ('./team/router')
 const userRouter = require ('./auth/router')
 
 const app = express()
-
+const jsonParser = bodyParser.json()
 
 app.use(teamRouter)
 app.use(jsonParser)
 app.use(userRouter)
 
-const port = process.env.PORT || 4100
-app.listen(port, () => `Listening on port ${port}`)
+app.get('/', (req, res) => res.send('hello there'))
 
-//push me gentley
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
